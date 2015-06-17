1030b91e6aad6d184ad86d97d6932f4037a1d923
Dmitry Novik <d1mas1k4@yandex.ru>
2015-06-17 20:58:10 +0300

    My vector implementation

--- my_vector.cpp
+++ my_vector.cpp
@@ -4,130 +4,110 @@
 
 #include "my_vector.h"
 
-void my_vector::push_back(size_t val) {
-    if (this->is_small) {
-        std::vector<size_t>* curr = new std::vector<size_t>;
-        (*curr).push_back(*this->data.val);
-        (*curr).push_back(val);
-        this->is_small = false;
-        delete this->data.val;
-        this->data.vals = curr;
-    } else {
-        if (this->data.vals.use_count() == 1) {
-            this->data.vals->push_back(val);
-        } else {
-            std::vector<size_t>* curr = new std::vector<size_t>;
-            for (size_t i = 0; i < this->data.vals->size(); ++i) {
-                curr->push_back((*this->data.vals)[i]);
-            }
-            curr->push_back(val);
-            this->data.vals = curr;
-        }
-    }
-}
-
 my_vector::my_vector(size_t val) {
-    *this->data.val = val;
+    *this->data->val = val;
 }
 
 my_vector::my_vector()
-    :my_vector(0) { }
+        :my_vector(0)
+{}
 
 my_vector::my_vector(std::vector<size_t> val) {
-    *this->data.vals = val;
+    *this->data->vals = val;
 }
 
 my_vector::~my_vector() {
-    delete this->data.vals;
-}
-
-size_t my_vector::pop_back() {
-    if (this->data.vals.use_count() == 1) {
-        size_t ans = this->data.vals->back();
-        this->data.vals->pop_back();
-        if (this->data.vals->size() == 1) {
-            this->is_small = true;
-            this->data.val = new ((*this->data.vals)[0]);
-            delete this->data.vals;
-        }
-        return ans;
-    } else {
-        std::vector<size_t>* curr = new std::vector<size_t>;
-        for (size_t i = 0; i < this->data.vals->size(); ++i) {
-            curr->push_back((*this->data.vals)[i]);
-        }
-        size_t ans = curr->back();
-        curr->pop_back();
-        this->data.vals = curr;
-        if (curr->size() == 1) {
-            this->is_small = true;
-            this->data.val = &(*curr)[0];
-            delete curr;
-        }
-        return ans;
-    }
-}
-
-size_t my_vector::back() const {
-    if (this->is_small) {
-        return *(this->data.val);
-    } else {
-        return *(this->data.vals->[0]);
-    }
-}
-
-size_t my_vector::size() const {
-    if (is_small) {
-        return 1;
-    } else {
-        return this->data.vals->size();
-    }
-}
-
-void my_vector::resize(size_t len) {
-    if (this->is_small) {
-
-    }
-}
-
-void my_vector::resize(size_t len, size_t val) {
-
+    delete this->data;
 }
 
 void my_vector::correct() {
-    if (!this->is_small && this->data.vals->size() == 1) {
-        this->data.val = new *this->data.vals->[0];
-        delete this->data.vals;
+    if (!this->is_small && this->data->vals->size() == 1) {
+        size_t* curr = new *this->data->vals->[0];
+        delete this->data->vals;
+        this->data->val = curr;
         this->is_small = true;
     }
 }
 
 void my_vector::make_own() {
-    if (this->is_small) {
-        if (this->data.val.use_count() > 1) {
-            this->data.val = new (*this->data.val);
-        }
-    } else {
-        if (this->data.vals.use_count() > 1) {
+    if (this->data.use_count() > 1) {
+        if (this->is_small) {
+            this->data->val = new size_t(*this->data->val);
+        } else {
             std::vector<size_t>* curr = new std::vector<size_t>;
-            for (size_t i = 0; i < this->data.vals->size(); ++i) {
-                curr->push_back(this->data.vals->[i]);
+            for (size_t i = 0; i < this->data->vals->size(); ++i) {
+                curr->push_back(this->data->vals->[i]);
             }
-            this->data.vals = curr;
+            this->data->vals = curr;
         }
     }
 }
 
-size_t& my_vector::operator[] (my_vector& v, const size_t index) {
-    if (v.is_small) {
-        if (v.data.val.use_count() > 1) {
-            v.make_own();
-        }
-        return *v.data.val;
+void my_vector::to_array() {
+    this->make_own();
+    if (this->is_small) {
+        std::vector<size_t>* tmp = new std::vector<size_t>;
+        tmp->push_back(*this->data->val);
+        delete this->data->val;
+        this->data->vals = tmp;
+    }
+}
+
+void my_vector::push_back(size_t val) {
+    this->make_own();
+    if (this->is_small) {
+        std::vector<size_t>* curr = new std::vector<size_t>;
+        curr->push_back(*this->data->val);
+        curr->push_back(val);
+        this->is_small = false;
+        delete this->data->val;
+        this->data->vals = curr;
     } else {
-        if (v.data.vals.use_count() > 1) {
-            v.make_own();
-        }
-        return (*v.data.vals)[index];
+        this->data->vals->push_back(val);
+    }
+}
+
+size_t my_vector::pop_back() {
+    this->make_own();
+    size_t ans = this->data->vals->back();
+    this->data->vals->pop_back();
+    this->correct();
+    return ans;
+}
+
+size_t my_vector::back() const {
+    if (this->is_small) {
+        return *this->data->val;
+    } else {
+        return this->data->vals->back();
+    }
+}
+
+size_t my_vector::size() const {
+    if (this->is_small) {
+        return 1;
+    } else {
+        return this->data->vals->size();
+    }
+}
+
+void my_vector::resize(size_t len) {
+    this->to_array();
+    this->data->vals->resize(len);
+    this->correct();
+}
+
+void my_vector::resize(size_t len, size_t val) {
+    this->to_array();
+    this->data->vals->resize(len, val);
+    this->correct();
+}
+
+size_t& my_vector::operator[] (my_vector& v, const size_t index) {
+    v.make_own();
+    if (v.is_small) {
+        return *v.data->val;
+    } else {
+        return *v.data->vals->[index];
     }
 }
--- my_vector.h
+++ my_vector.h
@@ -11,8 +11,8 @@
 #endif //BIGINT_MY_VECTOR_H
 
 union saved_data {
-    std::shared_ptr<size_t> val;
-    std::shared_ptr<std::vector<size_t>> vals;
+    size_t* val;
+    std::vector<size_t>* vals;
 };
 
 struct my_vector {
@@ -29,10 +29,11 @@ struct my_vector {
     size_t& operator[] (my_vector& v, const size_t index);
 
 private:
-    saved_data data;
-    bool sign, is_small;
+    std::shared_ptr<saved_data> data;
+    bool is_small;
     void correct();
     void make_own();
+    void to_array();
 };
 
 size_t& operator[] (my_vector& v, const size_t index);
